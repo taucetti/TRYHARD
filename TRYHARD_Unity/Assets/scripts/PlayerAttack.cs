eb080e29162d1fe737c2c3da6aae1d558dac9818
Michael Morales <156836539+toxinfang@users.noreply.github.com>
2024-01-25 21:11:10 -0600

    In Prpogress Attack
    Working on Attack Script

---
+++ TRYHARD_Unity/Assets/scripts/PlayerAttack.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            Attack();
+        }
+
+    }
+    Attack()
+    {
+        if (fullHands = true)
+        {
+
+        }
+        else
+        {
+            bearAttack();
+        }
+    }
+}
