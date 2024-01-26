c804def4967a8ac8b27560b71176e3057b03be60
Michael Morales <156836539+toxinfang@users.noreply.github.com>
2024-01-25 22:40:58 -0600

    Enemy Script
    Enemy Death Script

---
+++ TRYHARD_Unity/Assets/scripts/EnemyScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    private const string Player_Tag = "Player";
+
+    private const float Death_Radius = 5F;
+    private const float Death_Radius_SQ = Death_Radius * Death_Radius;
+
+    private GameObject playerGameObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerGameObject = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector3 distanceToPlayer = (playerGameObject.transform.position - this.transform.position);
+
+            if(distanceToPlayer.sqrMagnitude <= Death_Radius_SQ)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
